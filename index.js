1c429d192f1e0ce19d907f85b547953d786eb521
emerald-zzz <bantangsaran1011@gmail.com>
2023-11-04 11:58:02 +0100

    i removed the line for the constant cuz its no longer needed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const connectionPromise = require('./src/connexion.js');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const x=10;
+
 app.use(express.static('public'));
 
 app.listen(port, () => {
