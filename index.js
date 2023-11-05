60102584432bba59fba2ea7351e7a2c6b06fd7c5
emerald-zzz <bantangsaran1011@gmail.com>
2023-11-05 17:26:03 +0100

    adding items to the cart now works

--- index.js
+++ index.js
@@ -58,8 +58,46 @@ app.delete('/delete-product/:id', async (req, res) => {
       res.status(500).json({ error: 'Error deleting product' });
   }
 });
+// Endpoint to handle adding a product to the cart
+let cart = [];
+app.use(express.json());
 
+// Endpoint to get cart data
+app.get('/get-cart', (req, res) => {
+  res.json({ cart });
+});
 
+// Endpoint to add a product to the cart
+// Endpoint to add a product to the cart
+app.post('/add-to-cart', async (req, res) => {
+  const { productId, quantity } = req.body;
+
+  try {
+    const connection = await connectionPromise;
+    const product = await connection.get('SELECT id_produit as id, nom, chemin_image, prix FROM produit WHERE id_produit = ?', [productId]);
+
+    if (product) {
+      cart.push({
+        id: product.id,
+        name: product.nom,
+        price: product.prix,
+        quantity: parseInt(quantity),
+      });
+      res.json({ message: 'Product added to cart successfully' });
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  } catch (error) {
+    console.error('Error adding product to cart:', error);
+    res.status(500).json({ error: 'Error adding product to cart' });
+  }
+});
+// Endpoint to remove a product from the cart
+app.post('/remove-from-cart', (req, res) => {
+  const { productId } = req.body;
+  cart = cart.filter(item => item.id !== productId);
+  res.json({ message: 'Product removed from cart successfully' });
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
